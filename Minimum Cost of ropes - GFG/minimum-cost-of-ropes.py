1c4162fb34d1659b02911e3511691d3df5801357
Vasanth24 <81610373+VASANTH-KUMAR23@users.noreply.github.com>
2023-07-09 22:18:49 +0530

    Added solution - LeetHub

--- Minimum Cost of ropes - GFG/minimum-cost-of-ropes.py
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.py
@@ -7,10 +7,12 @@ class Solution:
         # code here
         heapq.heapify(arr)
         res = 0
-        
+       
         while len(arr) > 1:
             first = heapq.heappop(arr)
+            
             second = heapq.heappop(arr)
+            
             res += first + second
             heapq.heappush(arr,first+second)
         return res
