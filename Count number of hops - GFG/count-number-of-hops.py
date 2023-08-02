772f56f6f76082849f26d69a07913412a666c318
Vasanth24 <81610373+VASANTH-KUMAR23@users.noreply.github.com>
2023-08-02 18:25:36 +0530

    Added solution - LeetHub

--- Count number of hops - GFG/count-number-of-hops.py
+++ Count number of hops - GFG/count-number-of-hops.py
@@ -16,9 +16,9 @@ class Solution:
             return dp[step]
         
         dp = [-1]*(n+3)
-        #dp[n] = 1
-        #dp[n+1] = 0
-        #dp[n+2] = 0
+        dp[n] = 1
+        dp[n+1] = 0
+        dp[n+2] = 0
         return find(0,n,dp)
         
         
